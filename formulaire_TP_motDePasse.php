176d7e89f4ee3d7684892718374f099b3e47038d
François-Xavier GODIN <fx.godin@gmail.com>
2017-09-18 22:26:38 +0200

    Création formulaire pour TP page protégée par mot de passe

---
+++ formulaire_TP_motDePasse.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+     <head>
+          <title>Formulaire du TP Page protégée par mot de passe</title>
+          <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+     </head>
+     <body>
+
+     <form method="post" action="secret.php">
+     	<p>
+     		Veuillez entrer le mot de passe pour accéder à la page secrète
+     	<input type="password" name="motdepasse" label="Mot de passe"></input>
+     	<input type="submit" name="envoyer"></input>
+     	</p>
+     </form>
+
+
+
+
+
+
+
+     </body>
+</html>
