8ac678674b609a6cff79e3e1d9fbadac8e0672f5
François-Xavier GODIN <fx.godin@gmail.com>
2017-09-19 14:19:56 +0200

    ajout formulaire + traitement: fonctionnement ok

--- TP_motDePasse_1page.php
+++ TP_motDePasse_1page.php
@@ -6,13 +6,44 @@
      </head>
      <body>
 
-     
+     <?php
+
+     $mdp= 'kangourou';
+
+     //Si le mot de passe a été renseigné ET si il est correct....
+     if (isset($_POST['motdepasse']) AND $_POST['motdepasse']==$mdp)
+
+     //.. alors, on affiche les codes
+      {
+
+      echo 'Votre mot de passe est correct. <br/> Voici les codes : <br/>';
+                    //on génère des codes ...j'ai choisi des anagrammes du mdp pour le fun
+                    for ($i=0;$i<10;$i++)
+                    {
+                         $code=strtoupper(str_shuffle($mdp));
+                         $numero=$i+1;
+                         echo'Code n° '.$numero." = ".$code."<br/>";
+                    }
+      }
 
 
+     //.. SI NON, on affiche le formulaire
 
+     else {
 
+     ?>
 
+     <form method="post" action="TP_motDePasse_1page.php">
+          <p>
+               Veuillez entrer le mot de passe pour accéder à la page secrète
+          <input type="password" name="motdepasse" label="Mot de passe"></input>
+          <input type="submit" name="envoyer"></input>
+          </p>
+     </form>
 
+     <?php
+          } //fermeture du ELSE
+     ?>
 
      </body>
 </html>
