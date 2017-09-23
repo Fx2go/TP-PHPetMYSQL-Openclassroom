45858e5ce809cf21ffa975cb2bcfbe845241919d
François-Xavier GODIN <fx.godin@gmail.com>
2017-09-23 19:52:11 +0200

    ajout récupération des données en POST

--- minichat_post.php
+++ minichat_post.php
@@ -1,11 +1,16 @@
 <?php
 
 //On récupère les variables du pseudo et message envoyées par POST
+$pseudo=$_POST['pseudo'];
+$message=$_POST['message'];
 
-//On élimine d'éventuelles balises
+	//(On affiche les variables pour tester)
+echo $pseudo.'<br>';
+echo $message;
+//On élimine d'éventuelles balises html
 
 //On stock les variables en base de données
-
+	//On se connecte à la base de données
 //On retourne sur la page principale
 
 ?>
