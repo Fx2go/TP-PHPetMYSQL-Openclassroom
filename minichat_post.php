29547707e9b19b745e605e44fb6c80b426cfc2a4
François-Xavier GODIN <fx.godin@gmail.com>
2017-09-23 21:34:31 +0200

    ajout insertion en BDD

--- minichat_post.php
+++ minichat_post.php
@@ -1,16 +1,24 @@
 <?php
 
 //On récupère les variables du pseudo et message envoyées par POST
-$pseudo=$_POST['pseudo'];
-$message=$_POST['message'];
+//et on élimine d'éventuelles balises html
+$pseudo=strip_tags($_POST['pseudo']);
+$message=strip_tags($_POST['message']);
 
 	//(On affiche les variables pour tester)
-echo $pseudo.'<br>';
-echo $message;
-//On élimine d'éventuelles balises html
+	//echo $pseudo.'<br>';
+	//echo $message;
 
 //On stock les variables en base de données
 	//On se connecte à la base de données
+	include ('minichat_connection_BDD.php');
+	// On execute la requete pour stocker les variables
+	//$bdd->exec('INSERT INTO minichat(pseudo,message) VALUES(\'$pseudo\', \'$message\')');
+
+	$req = $bdd->prepare('INSERT INTO minichat(pseudo,message) VALUES(:pseudo,:message)');
+
+	$req->execute(array('pseudo'=>$pseudo, 'message'=> $message));
+
 //On retourne sur la page principale
 
 ?>
