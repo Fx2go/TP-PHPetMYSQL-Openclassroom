78691ce781a911ca3a568c67a588336ea09087eb
François-Xavier GODIN <fx.godin@gmail.com>
2017-09-23 19:24:22 +0200

    Ajout 2 fichiers pour réaliser le TP minichat

---
+++ minichat.php
@@ -0,0 +1,20 @@
+<?php
+
+//Formulaire 
+
+	// Champ "Pseudo"
+
+	// Champ "Message"
+
+
+	// Boutton "Envoyer"
+
+
+// Liste des messages ,du plus récent au plus ancien
+	// On va chercher dans la base minichat les messages
+	// On stock le résultat dasn un tableau
+	//On ferme le traitement de la requête
+	//On affiche les enregistrements du plus récent au plus ancien
+
+
+?>
---
+++ minichat_post.php
@@ -0,0 +1,11 @@
+<?php
+
+//On récupère les variables du pseudo et message envoyées par POST
+
+//On élimine d'éventuelles balises
+
+//On stock les variables en base de données
+
+//On retourne sur la page principale
+
+?>
