306519fe08201600bd79992e547d89be58784c0b
François-Xavier GODIN <fx.godin@gmail.com>
2017-09-23 19:51:26 +0200

    Ajout formulaire

--- minichat.php
+++ minichat.php
@@ -1,15 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>TP Minichat</title>
+        <meta charset="utf-8">
+        <meta name="description" content="Réalisation du TP mini-chat proposé sur la page https://openclassrooms.com/courses/concevez-votre-site-web-avec-php-et-mysql/tp-un-mini-chat">
+	</head>
+	<body>
+<!--Formulaire -->
+		<form method="post" action="minichat_post.php">
+			<p>	
+<!--Champ "Pseudo"-->
+				Pseudo :
+				<input type="text" name="pseudo" ></input><br/>
+<!--Champ "Message"-->
+				Message :
+				<input type="text" name="message" ></input><br/>
+
+<!--Boutton "Envoyer"-->
+				<input type="submit" name="Envoyer">
+			</p>
+		</form>
 <?php
-
-//Formulaire 
-
-	// Champ "Pseudo"
-
-	// Champ "Message"
-
-
-	// Boutton "Envoyer"
-
-
 // Liste des messages ,du plus récent au plus ancien
 	// On va chercher dans la base minichat les messages
 	// On stock le résultat dasn un tableau
@@ -18,3 +29,5 @@
 
 
 ?>
+	</body>
+</html>
