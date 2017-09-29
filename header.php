914703b596213d0cec62da05c3c3e2f0e230b352
François-Xavier GODIN <fx.godin@gmail.com>
2017-09-29 19:13:31 +0200

    codes réutilisés sur fichier inclus

---
+++ header.php
@@ -0,0 +1,1 @@
+<a href="."><h1> <i class="fa fa-bullhorn" aria-hidden="true"></i> Blog &amp; Coms</h1></a>
---
+++ titre_billet.php
@@ -0,0 +1,8 @@
+<a href="<?php echo $lien_article;?>">
+	<h3><?php echo htmlspecialchars($data['titre']);?>
+	<br/>
+	<span>
+		<?php echo htmlspecialchars($data['date_formatee']);?>
+	</span>
+	</h3>
+</a>
