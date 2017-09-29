4bace62f2b5e210f0c338e9ea8ef07491cc86bcc
François-Xavier GODIN <fx.godin@gmail.com>
2017-09-29 18:06:24 +0200

    ajouts commentaires associés

--- TP_Blog_billet.php
+++ TP_Blog_billet.php
@@ -25,9 +25,15 @@ include ('connection_BDD.php');
 		<title><?php echo $data['titre'] ?></title>
         <meta charset="utf-8">
         <meta name="description" content="">
+        <link href="https://fonts.googleapis.com/css?family=Geo|Dosis" rel="stylesheet" type="text/css" >
+        <link rel="stylesheet" href="style.css" />
+        <script src="https://use.fontawesome.com/3a7f9ca103.js"></script>
 	</head>
 	<body>
-<!-- -->
+	<a href="."><h1> <i class="fa fa-bullhorn" aria-hidden="true"></i> Blog &amp; Coms</h1></a>
+
+
+
 <div .news>
 			
 	<h3>
@@ -38,5 +44,40 @@ include ('connection_BDD.php');
 		
 	</p>
 </div>
+<?php
+}
+	//On ferme le traitement de la requête
+	$req->closeCursor();
+?>
+
+<div.commentaires>
+	<h4><i class="fa fa-commenting-o" aria-hidden="true"></i> Commentaires</h4>
+
+<?php
+		$req = $bdd->prepare("SELECT id,id_billet,nom_auteur,commentaire, DATE_FORMAT(date_commentaire, '%d/%m/%Y à %Hh %imin') AS date_formatee FROM commentaires WHERE id_billet= :id_billet ORDER BY id ");
+	// $req->execute(array('id_billet'=>$id_billet));
+	 
+	$req->bindParam(':id_billet', $id_billet);
+	$req->execute();
+
+	while ($data = $req->fetch())
+	{ 
+			echo'<p>';
+			echo'<strong>'.$data['nom_auteur'].'</strong>, le '.$data['date_formatee'];
+			echo' :" '.$data['commentaire'];
+			echo' "</p>';
+	}
+?>
+
+
+</div>
+
+		
+
+<?php
+
+	//On ferme le traitement de la requête
+	$req->closeCursor();
+?>
 	</body>
 </html>
