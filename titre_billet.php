a00955d3ac8153faf6e523a04cee8ca45a2d11fc
François-Xavier GODIN <fx.godin@gmail.com>
2017-10-05 14:17:29 +0200

    ajout boutons pour éditer billets

--- titre_billet.php
+++ titre_billet.php
@@ -1,8 +1,18 @@
-<a href="<?php echo $lien_article;?>">
-	<h3><?php echo htmlspecialchars($data['titre']);?>
-	<br/>
+<div class="titre">
+	<a href="<?php echo $lien_article;?>">
+		<h3><?php echo htmlspecialchars($data['titre']);?></h3>
+	</a>
 	<span>
 		<?php echo htmlspecialchars($data['date_formatee']);?>
 	</span>
-	</h3>
-</a>
+
+	<div class="edition">
+		<ul class="edition">
+			<li><a href="/admin/ajouter.php" title="Ajouter un nouveau billet"><i class="fa fa-plus-square" aria-hidden="true"></i></a></li>
+			<li><a href="/admin/modifier.php" title="Modifier ce billet"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></a></li>
+			<li><a href="/admin/supprimer.php" title="Supprimer ce billet !"><i class="fa fa-trash" aria-hidden="true"></i></a></li>
+		</ul>
+	</div>
+
+</div>
+
